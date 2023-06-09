28fb5f3dd5eb31417f58f61fa620bf62f31b3ec3
chiageng <chiageng12@gmail.com>
2023-06-09 14:21:37 +0800

    Add button from course to quiz

--- src/screens/MyCourseScreen.js
+++ src/screens/MyCourseScreen.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { Box, Typography, Container } from "@mui/material";
-import { neural500, neural900 } from "../design/color";
+import { Box, Typography, Container, Grid, Button } from "@mui/material";
+import { neural500, neural900, purplishBlue, white, purplishBlueDark } from "../design/color";
 import Topic from "../components/Topic";
 import { courses } from "../Courses";
+import { fontType } from "../design/font";
 
 function MyCourseScreen() {
   const params = useParams();
-  const course = courses.find(course => course._id === params.id);
+  const course = courses.find((course) => course._id === params.id);
   const topics = course.topics;
 
   const breadcrumb = (
@@ -42,25 +43,52 @@ function MyCourseScreen() {
   );
   return (
     <Container>
-    <Box pt={5} pb={10}>
-      <Typography
-        variant="h3"
-        fontFamily="Poppins"
-        sx={{
-          fontSize: 32,
-          fontWeight: 600,
-          fontStyle: "normal",
-          color: neural900,
-          mb: "32px",
-        }}
-      >
-        Video Topics
-      </Typography>
+      <Box pt={5} pb={10}>
       {breadcrumb}
-      {topics.map( topic => (
-        <Topic key={topic._id} topic={topic}/>
-      ))}
-    </Box>
+      {/* View Quiz button for webpage */}
+        <Grid container display="flex" justifyContent="space-between">
+          <Grid item>
+            <Typography
+              variant="h3"
+              fontFamily="Poppins"
+              sx={{
+                fontSize: 32,
+                fontWeight: 600,
+                fontStyle: "normal",
+                color: neural900,
+                mb: "32px",
+              }}
+            >
+              Video Topics
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              sx={{
+                backgroundColor: purplishBlue,
+                fontFamily: fontType,
+                color: white,
+                fontSize: 14,
+                width: "100%",
+                borderRadius: 3,
+                textDecoration: 'none',
+                ":hover": { backgroundColor: purplishBlueDark },
+              }}
+              // href={`/mycourses/${params.id}/myquiz`}
+              as={Link}
+              to={`/mycourses/${params.id}/myquiz`}
+            >
+              View Quiz
+            </Button>
+          </Grid>
+        </Grid>
+
+
+        
+        {topics.map((topic) => (
+          <Topic key={topic._id} topic={topic} />
+        ))}
+      </Box>
     </Container>
   );
 }
--- src/screens/MyQuizzesScreen.js
+++ src/screens/MyQuizzesScreen.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { Box, Typography, Container, Grid } from "@mui/material";
-import { neural500, neural900 } from "../design/color";
+import { Box, Typography, Container, Grid, Button } from "@mui/material";
+import { neural500, neural900, purplishBlue, purplishBlueDark, white } from "../design/color";
 import Topic from "../components/Topic";
 import { courses } from "../Courses";
 import QuizCard from "../components/QuizCard";
+import { fontType } from "../design/font";
 
 function MyQuizzesScreen() {
   const params = useParams();
@@ -44,19 +45,42 @@ function MyQuizzesScreen() {
   return (
     <Container>
       <Box pt={5} pb={10}>
-        <Typography
-          variant="h3"
-          fontFamily="Poppins"
-          sx={{
-            fontSize: 32,
-            fontWeight: 600,
-            fontStyle: "normal",
-            color: neural900,
-            mb: "32px",
-          }}
-        >
-          My Quizzes
-        </Typography>
+      <Grid container display="flex" justifyContent="space-between">
+          <Grid item>
+            <Typography
+              variant="h3"
+              fontFamily="Poppins"
+              sx={{
+                fontSize: 32,
+                fontWeight: 600,
+                fontStyle: "normal",
+                color: neural900,
+                mb: "32px",
+              }}
+            >
+              My Quizzes
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              sx={{
+                backgroundColor: purplishBlue,
+                fontFamily: fontType,
+                color: white,
+                fontSize: 14,
+                width: "100%",
+                borderRadius: 3,
+                ":hover": { backgroundColor: purplishBlueDark },
+                textDecoration: 'none'
+              }}
+              // href={`/mycourses/${params.id}`}
+              as={Link}
+              to={`/mycourses/${params.id}`}
+            >
+              View Lessons
+            </Button>
+          </Grid>
+        </Grid>
         {breadcrumb}
         <Grid container spacing={2}>
           {topics.map((topic) => (
